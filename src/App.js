a276314ad71075bfa643a971d3fce157987eb32a
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-04-02 12:23:10 -0300

    add tag main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import Menu_base from './components/menu.js';
+import MenuBase from './components/menu.js';
 
 function App() {
   return (
     <>
-      <Menu_base />
-      <div class="h-[100vh] w-full">
-        <div className="flex pl-10 justify-center h-[80vh] w-full bg-[var(--blue-bg)]">
-          <nav className="w-1/2 h-6 flex justify-between my-5">
+      {/* Menu Lateral */}
+      <MenuBase />
+      <div class="h-[100vh] w-[100vw]">
+        <div className="relative -z-50 flex flex-col pl-32 items-start justify-start h-[80vh] w-full bg-[var(--blue-bg)]">
+          {/* Menu Header */}
+          <nav className="ml-32 px-10 w-2/3 h-6 flex justify-between my-5">
             <div className="pb-3 w-20 text-center">
-              <a href="">SERVIÇOS</a>
+              <a href="/">SERVIÇOS</a>
             </div>
             <div className="pb-3 w-20 text-center">
-              <a href="">CONTATOS</a>
+              <a href="/">CONTATOS</a>
             </div>
             <div className="pb-3 w-28 text-center">
-              <a href="">OBSERVAÇÕES</a>
+              <a href="/">OBSERVAÇÕES</a>
             </div>
           </nav>
+          {/* Conteudos */}
+          <main className='absolute mt-20 ml-10 bg-black h-[75vh] w-[80vw]'>
+            <section id="home">
+
+            </section>
+          </main>
         </div>
       </div>
     </>
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-export default function Menu_base(){
+export default function MenuBase(){
     return(
         <section className="absolute top-8 h-[90vh] w-40 bg-[var(--blue)] rounded-2xl -left-5">
             <nav className="relative h-full p-12">
@@ -6,13 +6,13 @@ export default function Menu_base(){
                 <img className="absolute right-3 bottom-3" src="./img/vetores/vetor_menu_bot.png" alt="" width={100}/>
                 <ul className="h-full flex flex-col justify-around items-center">
                     <li className="z-10">
-                        <img className="cursor-pointer" src="./img/icons/menu-home.png" alt="" width={60}/>
+                        <img className="max-w-14 cursor-pointer" src="./img/icons/menu-home.png" alt="" width={60}/>
                     </li>
                     <li className="z-10">
-                        <img className="max-w-none cursor-pointer" src="./img/icons/menu-list.png" alt="" width={80}/>
+                        <img className="max-w-14 cursor-pointer" src="./img/icons/menu-list.png" alt="" width={80}/>
                     </li>
                     <li className="z-10">
-                        <img className="cursor-pointer" src="./img/icons/menu-perfil.png" alt="" width={60}/>
+                        <img className="max-w-14 cursor-pointer" src="./img/icons/menu-perfil.png" alt="" width={60}/>
                     </li>
                 </ul>
             </nav>
