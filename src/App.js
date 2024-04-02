5c1ea1f963bd57efdd70bec49dff6aebc8763039
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-04-02 11:53:43 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import Menu_base from './components/menu.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Menu_base />
+      <div class="h-[100vh] w-full">
+        <div className="flex pl-10 justify-center h-[80vh] w-full bg-[var(--blue-bg)]">
+          <nav className="w-1/2 h-6 flex justify-between my-5">
+            <div className="pb-3 w-20 text-center">
+              <a href="">SERVIÇOS</a>
+            </div>
+            <div className="pb-3 w-20 text-center">
+              <a href="">CONTATOS</a>
+            </div>
+            <div className="pb-3 w-28 text-center">
+              <a href="">OBSERVAÇÕES</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/menu.js
@@ -0,0 +1,21 @@
+export default function Menu_base(){
+    return(
+        <section className="absolute top-8 h-[90vh] w-40 bg-[var(--blue)] rounded-2xl -left-5">
+            <nav className="relative h-full p-12">
+                <img className="absolute right-3 top-3" src="./img/vetores/vetor_menu_top.png" alt="" width={100}/>
+                <img className="absolute right-3 bottom-3" src="./img/vetores/vetor_menu_bot.png" alt="" width={100}/>
+                <ul className="h-full flex flex-col justify-around items-center">
+                    <li className="z-10">
+                        <img className="cursor-pointer" src="./img/icons/menu-home.png" alt="" width={60}/>
+                    </li>
+                    <li className="z-10">
+                        <img className="max-w-none cursor-pointer" src="./img/icons/menu-list.png" alt="" width={80}/>
+                    </li>
+                    <li className="z-10">
+                        <img className="cursor-pointer" src="./img/icons/menu-perfil.png" alt="" width={60}/>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
